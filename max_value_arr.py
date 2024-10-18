94ab9e43d01c57b1042c7e79ea1974d545665045
NEHA81488 <neha_22aib11@kgkite.ac.in>
2024-10-18 16:52:29 +0530

    maxvalarray

--- max_value_arr.py
+++ max_value_arr.py
@@ -1,23 +1,18 @@
-#APPROACH -1
-#SORTING THE INITIAL LIST AND RETURNING THE LAST ELEMENT OF LIST
-def max(arr):
 
+def find_max(arr):
     arr.sort()
     return arr[-1]
-#APPROACH -2
-#ITERATING THROUGH ALL THE ELEMENTS IN LIST AND COMPARING EACH ELEMENT
 
-def max(arr):
-    
+
+# Iterating through all the elements in the list and comparing each element
+def find_max(arr):
     max_element = arr[0]
-
-    for i in range(len(arr)):
+    for i in range(1, len(arr)):
         if arr[i] > max_element:
             max_element = arr[i]
+    return max_element
 
-    return (max_element)
 
-#APPROACH-3
-#USING INBUILT FUNCTION
-def max(arr):
-    return (max(arr))
+# Using the in-built max() function
+def find_max(arr):
+    return max(arr)
